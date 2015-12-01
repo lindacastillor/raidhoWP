86ab1c7d154fc7a233b9933acff22cbdecd07f8d
raidho-mx <miranda@raidho.mx>
2015-12-01 16:55:46 -0600

    Added 2 levels of Projects and override main loop to take out first two rows

--- wp-content/themes/voyage/archive-work.php
+++ wp-content/themes/voyage/archive-work.php
@@ -11,33 +11,70 @@ get_header();
 
 <section id="recent_work">
 
-	<div class="wrap">
+	<div class="wrap project_wrapper"><?php
 
-		<h2 class="Decima">Recent Work</h2>
 
-<?php /*
+		$two_obj = get_field('two_featured', 4196);
+
+		if($two_obj): ?>
 		<div class="bl-party-two-w-captions">
-	        <ul>
-	            <li>
-					<a href="#">
-						<img src="http://lorempixel.com/380/380/technics">
-		                <p>Penguin Books. <span class="Leitura">An Expanding Universe Keynote.</span></p>
-					</a>
-	            </li>
-	        <ul>
-	    </div>
+			<ul><?php
+			foreach ($two_obj as $post) :
+				setup_postdata($post); ?>
 
-	    <div class="bl-party-three-w-captions">
-	        <ul>
-	            <li>
-	                <a href="#">
-						<img src="http://lorempixel.com/380/380/technics">
-		                <p>Penguin Books. <span class="Leitura">An Expanding Universe Keynote.</span></p>
+				<li>
+					<a href="<?php the_permalink(); ?>"><?php
+
+						if (has_post_thumbnail( $post->ID ) ):
+						$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'med-sq' ); ?>
+							<img src="<?php echo $image[0]; ?>" /><?php
+						endif; ?>
+
+						<p><?php the_title(); ?> <span class="Leitura"><?php the_field('subtitle'); ?></span></p>
 					</a>
-	            </li>
-	        <ul>
-	    </div>
-*/
+				</li><?php
+				$two_ids[] .= $post->ID;
+
+			endforeach;
+			wp_reset_postdata(); ?>
+			<ul>
+		</div><?php
+		endif;
+
+
+
+
+		$three_obj = get_field('three_featured', 4196);
+
+		if($three_obj): ?>
+		<div class="bl-party-three-w-captions">
+			<ul><?php
+			foreach ($three_obj as $post) :
+				setup_postdata($post); ?>
+
+				<li>
+					<a href="<?php the_permalink(); ?>"><?php
+
+						if (has_post_thumbnail( $post->ID ) ):
+						$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'med-sq' ); ?>
+							<img src="<?php echo $image[0]; ?>" /><?php
+						endif; ?>
+
+						<p><?php the_title(); ?> <span class="Leitura"><?php the_field('subtitle'); ?></span></p>
+					</a>
+				</li><?php
+				$two_ids[] .= $post->ID;
+
+			endforeach;
+			wp_reset_postdata(); ?>
+			<ul>
+		</div><?php
+		endif;
+
+
+
+
+		// Excludes Case Studies selected above (view functions.php-Line31)
 
 		if(have_posts()): ?>
 			<div class="bl-party-four-w-captions">
--- wp-content/themes/voyage/functions.php
+++ wp-content/themes/voyage/functions.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 
 
-// 	Theme Supports 
+// 	Theme Supports
 
 	add_editor_style('css/wysiwyg.css');
 	add_theme_support( 'post-thumbnails' );
@@ -11,21 +11,42 @@
 
 
 /*
- *  ACF - Options  
+ *  ACF - Options
  */
 
-	if( function_exists('acf_add_options_page') ) { 
+	if( function_exists('acf_add_options_page') ) {
 		acf_add_options_page(array(
 			'page_title' 	=> 'General Options',
 			'menu_title'	=> 'Options',
 			'menu_slug' 	=> 'theme-general-settings',
 			'capability'	=> 'edit_posts',
 			'redirect'		=> false
-		)); 
+		));
 	}
 
 
 
 
+/*
+ *  Exclude previously selected Case Studies
+ */
 
+	function exclude_single_posts_home($query) {
+		if ($query->is_archive('work') && $query->is_main_query()) {
 
+			$two_obj = get_field('two_featured', 4196);
+			foreach ($two_obj as $post) :
+				setup_postdata($post);
+				$two_ids[] .= $post->ID;
+			endforeach;
+
+			$three_obj = get_field('three_featured', 4196);
+			foreach ($three_obj as $post) :
+				setup_postdata($post);
+				$two_ids[] .= $post->ID;
+			endforeach;
+
+			$query->set('post__not_in', $two_ids);
+		}
+	}
+	add_action('pre_get_posts', 'exclude_single_posts_home');
