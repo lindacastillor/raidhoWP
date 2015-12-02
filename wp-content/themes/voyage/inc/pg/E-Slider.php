6d6496bddc3d811f8a79b51f39d55df9e6bfa153
raidho-mx <miranda@raidho.mx>
2015-12-02 11:17:01 -0600

    Major Structure fixes

--- wp-content/themes/voyage/inc/pg/E-Slider.php
+++ wp-content/themes/voyage/inc/pg/E-Slider.php
@@ -1,62 +1,91 @@
-<script type="text/javascript">
-	$(function () {
-		$(".tabbed-slider").responsiveSlides({
-			pager: false,
-			auto: false,
-			prevText: "-",
-			nextText: "-",
-			nav: true,
-			manualControls: '.tabs-pager'
-		});
-	});
-</script>
+<?php
 
-<section class="main_banner gray_light_bg">
+/*
+Slider with Tabs
+Neue Raidho Website
+*/
 
-	<ul class="tabs-pager"><?php
-		$tabN = 1;
-		while(have_rows('tabs')) {
-			the_row();
-			echo '<li><a href="#tab'.$tabN++.'"><p>'.get_sub_field('title').'</p></a></li>';
-		} ?>
-	</ul>
-
-	<!-- slides container -->
+?>
+<div class="gray_light_bg section_pad">
 	<div class="wrap">
-		<ul class="tabbed-slider"><?php
-			$eleN = 1;
-			while (have_rows('tabs')) {
-				the_row(); ?>
-				<li><?php
+		<div>
 
-				global $titBgID;
-				$img = get_sub_field('img');
+			<script type="text/javascript">
+				$(function () {
+					// $(".tabbed-slider").responsiveSlides({
+					// 	pager: false,
+					// 	auto: false,
+					// 	prevText: "-",
+					// 	nextText: "-",
+					// 	nav: true,
+					// 	manualControls: '.tabs-pager'
+					// });
+					$("#slider1").responsiveSlides({
+						pager: false,
+						auto: false,
+						prevText: "-",
+						nextText: "-",
+						nav: true,
+						manualControls: '#slider1-pager'
+					});
+				});
+			</script>
 
-				if($img){
-					$img_large = wp_get_attachment_image_src($img, 'large');
-					$img_larger = wp_get_attachment_image_src($img, 'larger');
-					$img_largest = wp_get_attachment_image_src($img, 'largest');
-
-					echo '<style> #slbg-'.$titBgID.' {background-image: url(' . $img_large[0] . ');}';
-					if($img_large) { echo ' @media (min-width: 1024px) { #slbg-'.$titBgID.' {background-image: url(' . $img_larger[0] . ');} }'; }
-					if($img_larger) { echo ' @media (min-width: 1400px) { #slbg-'.$titBgID.' {background-image: url(' . $img_largest[0] . ');} }'; }
-					echo '</style>';
-
-					echo '<div class="image" id="slbg-'.$titBgID++.'"></div>';
-				}  ?>
-
-
-					<div class="txt white_bg">
-						<div>
-							<h2><span class="red"><?php the_sub_field('title'); ?>:</span> <?php the_sub_field('heading'); ?></h2>
-							<p class="Decima"><?php the_sub_field('content'); ?></p><?php
-							// if is post object:
-							// <p><span class="Decima gray">Read the full story</span></p> ?>
-						</div>
-					</div>
-				</li><?php
+			<?php if(get_sub_field('title')){
+				echo '<h3 class="Decima">'.get_sub_field('title').'</h3>';
 			} ?>
-		</ul>
 
+
+			<div class="main_banner gray_light_bg">
+
+				<ul class="tabs-pager" id="slider1-pager"><?php
+					$tabN = 1;
+					while(have_rows('tabs')) {
+						the_row();
+						echo '<li><a href="#tab'.$tabN++.'"><p>'.get_sub_field('title').'</p></a></li>';
+					} ?>
+				</ul>
+
+				<!-- slides container -->
+				<div class="slider_container">
+					<ul id="slider1"><?php
+						$eleN = 1;
+						while (have_rows('tabs')) {
+							the_row(); ?>
+							<li><?php
+
+							global $titBgID;
+							$img = get_sub_field('img');
+
+							if($img){
+								$img_large = wp_get_attachment_image_src($img, 'large');
+								$img_larger = wp_get_attachment_image_src($img, 'larger');
+								$img_largest = wp_get_attachment_image_src($img, 'largest');
+
+								echo '<div class="image" id="slbg-'.$titBgID.'">
+									<style> #slbg-'.$titBgID.' {background-image: url(' . $img_large[0] . ');}';
+								if($img_large) { echo ' @media (min-width: 1024px) { #slbg-'.$titBgID.' {background-image: url(' . $img_larger[0] . ');} }'; }
+								if($img_larger) { echo ' @media (min-width: 1400px) { #slbg-'.$titBgID++.' {background-image: url(' . $img_largest[0] . ');} }'; }
+								echo '</style>
+								</div>';
+
+							}  ?>
+
+
+								<div class="txt white_bg">
+									<div>
+										<h2><span class="red"><?php the_sub_field('title'); ?>:</span> <?php the_sub_field('heading'); ?></h2>
+										<p class="Decima"><?php the_sub_field('content'); ?></p><?php
+										// if is post object:
+										// <p><span class="Decima gray">Read the full story</span></p> ?>
+									</div>
+								</div>
+							</li><?php
+						} ?>
+					</ul>
+
+				</div>
+			</div>
+		</div>
 	</div>
-</section>
+</div>
