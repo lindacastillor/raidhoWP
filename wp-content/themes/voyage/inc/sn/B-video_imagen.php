4b4a3a89fc16a4e0698e71c0ab59bf3f113cf66f
Linda Castillor <linda@raidho.mx>
2016-03-14 17:50:44 -0600

    Enable vimeo video and image flex-block

--- wp-content/themes/voyage/inc/sn/B-video_imagen.php
+++ wp-content/themes/voyage/inc/sn/B-video_imagen.php
@@ -1,45 +1,50 @@
 <?php
 
-if (get_sub_field('choose') == 'video_imagen') :
-	echo 'one: images';
-	$images = get_sub_field('video_foto');
-	if($images) : ?>
-		<div class="bl-party-one-no-captions"><?php
-			foreach( $images as $image ): ?>
-				<div>
-					<picture>
-						<img class="full_width_image"
-							 src="<?php echo $image['sizes']['larger']; ?>"
-							 <?php echo tevkori_get_srcset_string( $image['ID'], 'largest' ); ?>
-							 alt="<?php echo $image['alt']; ?>" />
-					</picture>
-				</div><?php
-			endforeach;
-			if(get_sub_field('caption')){ ?>
-				<div class="caption decima"><?php the_sub_field('caption') ?></div><?php
-			} ?>
+if (get_sub_field('choose') == 'video_foto') : ?>
+		<div class="bl-party-two-no-captions">
+			<div>
+				<style>
+				.embed-container { position: relative; padding-bottom: 57%; height: 0;max-width: 100%; margin-right: 0!important;margin-left: 0!important;width: 100%!important;}
+				.embed-container iframe,
+				.embed-container object,
+				.embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }
+				</style>
+				<div class='embed-container'>
+					<?php the_sub_field('video') ?>
+				</div>
+			</div>
+			<div>
+				<picture>
+					<?php
+					$image = get_sub_field('foto');
+					if( !empty($image) ): ?>
+						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					<?php endif; ?>
+				</picture>
+			</div>
 		</div><?php
-	endif;
 
-elseif (get_sub_field('choose') == 'foto_video') :
-	// echo 'two: images';
-	$images = get_sub_field('gallery');
-	if($images) : ?>
-		<div class="bl-party-two-no-captions"><?php
-			foreach( $images as $image ): ?>
-				<div>
-					<picture>
-						<img
-							 src="<?php echo $image['sizes']['larger']; ?>"
-							 <?php echo tevkori_get_srcset_string( $image['ID'], 'largest' ); ?>
-							 alt="<?php echo $image['alt']; ?>" />
-					</picture>
-				</div><?php
-			endforeach;
-			if(get_sub_field('caption')){ ?>
-				<div class="caption decima"><?php the_sub_field('caption') ?></div><?php
-			} ?>
+elseif (get_sub_field('choose') == 'foto_video') : ?>
+		<div class="bl-party-two-no-captions" style="display: table;width: 100%;">
+			<div>
+				<picture>
+					<?php
+					$image = get_sub_field('foto');
+					if( !empty($image) ): ?>
+						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					<?php endif; ?>
+				</picture>
+			</div>
+			<div>
+				<style>
+				.embed-container { position: relative; padding-bottom: 57%; height: 0;max-width: 100%; margin-right: 0!important;margin-left: 0!important;width: 100%!important;}
+				.embed-container iframe,
+				.embed-container object,
+				.embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }
+				</style>
+				<div class='embed-container'>
+					<?php the_sub_field('video') ?>
+				</div>
+			</div>
 		</div><?php
-	endif;
-
 endif; ?>
