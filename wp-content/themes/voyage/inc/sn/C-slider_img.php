78077cb18894b5cefaa2d241fa218973b08535a0
Linda Castillor <linda@raidho.mx>
2016-03-15 17:04:46 -0600

    Clean extra and paste to shame, fix structure img_slider structure

--- wp-content/themes/voyage/inc/sn/C-slider_img.php
+++ wp-content/themes/voyage/inc/sn/C-slider_img.php
@@ -1,55 +1,59 @@
 <?php
 if (get_sub_field('choose') == 'slider-right') : ?>
-	<div>
-		<picture>
+	<div class="bl-party-img-slider">
+		<div>
+			<picture>
+				<?php
+				$image = get_sub_field('img');
+				if( !empty($image) ): ?>
+					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+				<?php endif; ?>
+			</picture>
+		</div>
+		<div>
 			<?php
-			$image = get_sub_field('img');
-			if( !empty($image) ): ?>
-				<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-			<?php endif; ?>
-		</picture>
-	</div>
-	<div>
-		<?php
-		$images = get_sub_field('gallery');
-		if($images) : ?>
-			<div class="wide_slider">
-				<ul class="rslides slider2"><?php
-					foreach( $images as $image ): ?>
-						<li>
-							<div class="image" style="background-image: url(<?php echo $image['sizes']['larger']; ?>);"></div>
-						</li><?php
-					endforeach;	?>
-				<ul>
-			</div><?php
-		endif; ?>
+			$images = get_sub_field('gallery');
+			if($images) : ?>
+				<div class="wide_slider">
+					<ul class="rslides slider2"><?php
+						foreach( $images as $image ): ?>
+							<li>
+								<div class="image" style="background-image: url(<?php echo $image['sizes']['larger']; ?>);"></div>
+							</li><?php
+						endforeach;	?>
+					<ul>
+				</div><?php
+			endif; ?>
+		</div>
 	</div>
 
 <?php
 elseif (get_sub_field('choose') == 'slider-left') : ?>
-	<div>
-		<?php
-		$images = get_sub_field('gallery');
-		if($images) : ?>
-			<div class="wide_slider">
-				<ul class="rslides slider2"><?php
-					foreach( $images as $image ): ?>
-						<li>
-							<div class="image" style="background-image: url(<?php echo $image['sizes']['larger']; ?>);"></div>
-						</li><?php
-					endforeach;	?>
-				<ul>
-			</div><?php
-		endif; ?>
-	</div>
-	<div>
-		<picture>
+	<div class="bl-party-img-slider">
+		<div>
 			<?php
-			$image = get_sub_field('img');
-			if( !empty($image) ): ?>
-				<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-			<?php endif; ?>
-		</picture>
+			$images = get_sub_field('gallery');
+			if($images) : ?>
+				<div class="wide_slider">
+					<ul class="rslides slider2"><?php
+						foreach( $images as $image ): ?>
+							<li>
+								<div class="image" style="background-image: url(<?php echo $image['sizes']['larger']; ?>);"></div>
+							</li><?php
+						endforeach;	?>
+					<ul>
+				</div><?php
+			endif; ?>
+		</div>
+		<div>
+			<picture>
+				<?php
+				$image = get_sub_field('img');
+				if( !empty($image) ): ?>
+					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+				<?php endif; ?>
+			</picture>
+		</div>
 	</div>
 <?php
 endif; ?>
