00763370cccefee0a9f1eb79d5c873d799d899fb
Linda Castillor <linda@raidho.mx>
2016-03-15 16:20:35 -0600

    Add styles and scripts for slider

--- wp-content/themes/voyage/js/scripts.js
+++ wp-content/themes/voyage/js/scripts.js
@@ -6,3 +6,44 @@ $(document).keypress(function(e){
 		$("#dev_nav").toggle();
 	}
 });
+
+$(function () {
+	$("#slider1").responsiveSlides({
+		pager: false,
+		auto: false,
+		prevText: "-",
+		nextText: "-",
+		nav: true,
+		manualControls: '#slider1-pager'
+	});
+
+	$(".slider2").responsiveSlides({
+		pager: true,
+		auto: false,
+		nav: true,
+		namespace: "centered-btns"
+	});
+
+	$("#slider3").responsiveSlides({
+		pager: false,
+		auto: false,
+		prevText: "-",
+		nextText: "-",
+		nav: true,
+		manualControls: '#slider3-pager'
+	});
+});
+
+$(".fancybox").fancybox({
+	prevEffect	: 'false',
+	nextEffect	: 'false',
+	maxHeight	: 700,
+	fitToView	: true,
+	width		: '100%',
+	arrows		: false,
+	autoSize	: false,
+	closeBtn	: false,
+	closeClick	: false,
+	openEffect	: 'fade',
+	closeEffect	: 'fade'
+});
