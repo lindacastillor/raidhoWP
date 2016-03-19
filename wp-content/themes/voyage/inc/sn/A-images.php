eb8cb3ec0427f33ab08ae040b95d809f1179de9e
Linda Castillor <linda@raidho.mx>
2016-03-18 18:58:55 -0600

    Enable a third middle slider

--- wp-content/themes/voyage/inc/sn/A-images.php
+++ wp-content/themes/voyage/inc/sn/A-images.php
@@ -157,6 +157,54 @@ elseif (get_sub_field('choose') == 'thirds-slider-left') : ?>
 			endif; ?>
 		</div><?php
 
+elseif (get_sub_field('choose') == 'thirds-slider-middle') : ?>
+		<div class="bl-party-img-slider-third sldr-middle">
+			<div class="slider">
+				<div>
+					<picture>
+						<img src="<?php echo $image['sizes']['larger']; ?>"
+							 <?php echo tevkori_get_srcset_string( $image['ID'], 'largest' ); ?>
+							 alt="<?php echo $image['alt']; ?>" />
+					</picture>
+				</div>
+			</div>
+			<div class="slider">
+				<style>
+					.bl-party-img-slider-third.sldr-middle .slider ul.centered-btns_tabs li a{
+						border: 4px solid <?php the_sub_field('bg-color'); ?>;}
+					.bl-party-img-slider-third.sldr-middle .slider a.centered-btns_nav.next:hover,
+					.bl-party-img-slider-third.sldr-middle .slider a.centered-btns_nav.prev:hover{
+						background-color: <?php the_sub_field('bg-color'); ?>;}
+					.bl-party-img-slider-third.sldr-middle .slider ul.centered-btns_tabs li a:hover,
+					.bl-party-img-slider-third.sldr-middle .slider ul.centered-btns_tabs li.centered-btns_here a{
+						background-color: <?php the_sub_field('bg-color'); ?>;
+						border: 4px solid <?php the_sub_field('bg-color'); ?>;}
+				</style>
+				<?php
+				$images = get_sub_field('gallery');
+				if($images) : ?>
+					<div class="wide_slider">
+						<ul class="rslides slider2"><?php
+							foreach( $images as $image ): ?>
+								<li>
+									<img src="<?php echo $image['sizes']['larger']; ?>" alt="<?php echo $image['alt']; ?>" />
+								</li><?php
+							endforeach;	?>
+						</ul>
+					</div><?php
+				endif; ?>
+			</div>
+			<div class="slider">
+				<div>
+					<picture>
+						<img src="<?php echo $image['sizes']['larger']; ?>"
+							 <?php echo tevkori_get_srcset_string( $image['ID'], 'largest' ); ?>
+							 alt="<?php echo $image['alt']; ?>" />
+					</picture>
+				</div>
+			</div>
+		</div><?php
+
 elseif (get_sub_field('choose') == 'thirds-slider-right') : ?>
 		<div class="bl-party-img-slider-third sldr-right">
 			<?php
